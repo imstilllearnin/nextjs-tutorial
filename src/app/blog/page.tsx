393e5698b1e31a475b9ac2af3a2cba9bd41e5a0d
Mustafa <mustafa.cgdm1@gmail.com>
2025-02-09 21:32:10 +0300

    Title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog", // you can like turn this into an object and like set the absolute: to whatever to ignore the template purrrr
+};
+
 export default function Blog() {
-    return <h1>My Blog</h1>
+  return <h1>My Blog</h1>;
 }

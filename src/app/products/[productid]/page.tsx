488725cc5209b60ed894b717cfd5097a87d4d79a
Mustafa <mustafa.cgdm1@gmail.com>
2025-02-09 21:06:20 +0300

    Routing Metadata

--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -1,15 +1,27 @@
-export default async function Page({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{
     productid: string;
   }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { productid } = await params;
+
+  return {
+    title: `Product ${productid}`,
+  };
+};
+
+export default async function ProductDetails({ params }: Props) {
   const { productid } = await params;
 
   return (
     <>
-      <h1>Details About Product {productid}</h1>;
+      <h1>Details About Product {productid}</h1>
     </>
   );
 }

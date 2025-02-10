c4cdf4e58e26aef4b8b1ec9b59aaa2e6a6cf2e7d
Mustafa <mustafa.cgdm1@gmail.com>
2025-02-10 17:22:34 +0300

    added shadcnui sidebar and buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>
+    <div className="rounded-md">
       <h1 className="text-3x1 font-bold underline">Welcome Home Bitch!</h1>
-      <Link href="/blog">Blog</Link>
-      <hr />
-      <Link href="/products">products</Link>
-    </>
+      <ul>
+        <li>
+          <Button className="m-4">
+            <Link href="/blog">Blog</Link>
+          </Button>
+        </li>
+        <li>
+          <Button className="m-4">
+            <Link href="/products">products</Link>
+          </Button>
+        </li>
+      </ul>
+    </div>
   );
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,83 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import Link from "next/link";
+
+import {
+  Sidebar,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar";
+
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "@/components/ui/collapsible";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Support",
+    url: "/support",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar variant="floating" collapsible="icon" className="p-4 pr-0">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Sidebar</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+              <Collapsible defaultOpen className="group/collapsible">
+                {/*collapsing item */}
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton />
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      {/*sidebarmenuitems go in here*/}
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+              {/*collapsing item */}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}

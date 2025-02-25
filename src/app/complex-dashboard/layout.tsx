0ddecaa14a1c0054999a4e106fbbee3526774004
Mustafa <mustafa.cgdm1@gmail.com>
2025-02-25 23:43:54 +0300

    Conditional Routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -1,24 +1,45 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 export default function ComplexDashboardLayout({
   children,
   users,
   revenue,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
-  return (
+  const [LoggedIn, setLoggedIn] = useState(false);
+
+  return LoggedIn ? (
     <div>
       <div>{children}</div>
-      <div className="flex">
+      <div className="flex sm:flex-col lg:flex-row">
         <div className="flex flex-col">
           <div>{users}</div>
           <div>{revenue}</div>
         </div>
-        <div className="flex flex-1">{notifications}</div>
+
+        <div className="flex lg:flex-row sm:flex-col">{notifications}</div>
       </div>
+    </div>
+  ) : (
+    <div className="text-center">
+      {login}
+      <Button
+        onClick={() => {
+          setLoggedIn(true);
+        }}
+      >
+        Login
+      </Button>
     </div>
   );
 }
